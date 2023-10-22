9637e8709fd13ff7d10b8c6712264baa79385377
VowcicefskiJ <96920791+VowcicefskiJ@users.noreply.github.com>
2023-10-22 19:34:38 -0400

    Add files via upload

---
+++ AssignmentOperators/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int num1 {10};
+    int num2 {20};
+    
+    num1=num2=1000;
+    
+    cout << "num1 is " << num1<<endl;
+    cout << "
+    num2 is " << num2<<endl;
+    
+    cout<<endl;
+    return 0;
+}
